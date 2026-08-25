f4802c769c87e5ca287b5c50ae2f510bdadfc091
joaopedro-devbr <joaopedroandrade712@gmail.com>
2026-08-25 16:31:45 -0300

    Delete Python/condicionais directory

--- Python/condicionais/consumo_medio.py
+++
@@ -1,9 +0,0 @@
-# Exercício: Consumo médio
-# Objetivo: calcular o consumo médio de um veículo a partir da distância percorrida e do combustível utilizado.
-
-distanciaPercorrida = int(input("Distancia percorrida: "))
-combustivelGasto = float(input("Combustível gasto: "))
-
-consumoMedio = distanciaPercorrida / combustivelGasto
-
-print(f"Consumo medio = {consumoMedio:.3f}")
