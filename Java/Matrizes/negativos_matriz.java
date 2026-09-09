bd3ad7547ec43e81dd191dc2f7651ed7f85322d5
joaopedro-devbr <joaopedroandrade712@gmail.com>
2026-09-08 22:18:01 -0300

    adiciona exercicio para filtrar e exibir valores negativos de uma matriz

---
+++ Java/Matrizes/negativos_matriz.java
@@ -0,0 +1,39 @@
+// Exercício: Negativos Matriz
+// Objetivo: Ler uma matriz e imprimir na tela todos os valores negativos presentes nela.
+
+import java.util.Scanner;
+
+public class negativos_matriz {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        int n, m;
+
+        System.out.print("Qual a quantidade de linhas da matriz ? ");
+        m = sc.nextInt();
+        System.out.print("Qual a quantidade de colunas da matriz ? ");
+        n = sc.nextInt();
+
+        int[][] mat = new int[m][n];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print("Elemento [" + i + "," + j + "]: ");
+                mat[i][j] = sc.nextInt();
+            }
+        }
+
+        System.out.println("VALORES NEGATIVOS: ");
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (mat[i][j] < 0) {
+                    System.out.println(mat[i][j]);
+                }
+            }
+        }
+
+        sc.close();
+    }
+}
